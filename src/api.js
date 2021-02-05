46c3029d97946c93f01b138a6e73ff34fccfba81
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2021-02-05 10:51:50 +0100

    aggiunto qualche commento

--- src/api.js
+++ src/api.js
@@ -5,6 +5,7 @@ var usersRouter = require('./routes/users');
 const app = express();
 const router = express.Router();
 
+//Possiamo dichiareare una route qui
  router.get('/', (req, res) =>
  {
      res.json(
@@ -13,8 +14,18 @@ const router = express.Router();
      })
  });
 
+ //E' necessario usare per tutte le route il prefisso /.netlify/functions
  app.use('/.netlify/functions/api',router)
+ //E' possibile anche usare un file esterno
  app.use('/.netlify/functions/api/users', usersRouter);
+
+ //NB!
+ //NON è necessario avviare il server perchè stiamo creando una lambda function
+ /*
+ app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+*/
 
 module.exports = app;
 module.exports.handler= serverless(app); 
